64d882b8085f60d52faa3e74a4653debb0ee09cc
DigiD3ar <jmworking92@gmail.com>
2018-03-12 05:02:13 -0600

    added vars to ghost.hx

--- source/ghost.hx
+++ source/ghost.hx
@@ -1,7 +1,9 @@
 class Player extends FlxSprite {
+
+  private static inline var ACCELERATION:Int = 320; private static inline var DRAG:Int = 320;
+private static inline var GRAVITY:Int = 600; private static inline var JUMP_FORCE:Int = -280; private static inline var WALK_SPEED:Int = 80; private static inline var RUN_SPEED:Int = 140; private static inline var FALLING_SPEED:Int = 300;
       public function new()
-      {
-super(); }
+      {super(); }
 override public function update(elapsed:Float):Void {
           super.update(elapsed);
       }
